18187bebac417756c401e4e12a4450dbf2ffd699
Luan Hoang <luandnh1998@gmail.com>
2019-11-18 15:26:23 +0700

    Uncomment AuditLog Post

--- Nesops.Monitor.Log/Controllers/AuditLogController.cs
+++ Nesops.Monitor.Log/Controllers/AuditLogController.cs
@@ -41,13 +41,10 @@ namespace Nesops.Monitor.Log.Controllers
         [HttpPost]
         public async Task<ActionResult<AuditLogsReadModel>> Create([FromBody]AuditLogsCreateModel createModel, CancellationToken cancellationToken)
         {
-            //var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            //createModel.SysId = Guid.Parse(userId);
-            //var readModel = await CreateModel(createModel, cancellationToken);
-            //return CreatedAtAction(nameof(Get), new { id = readModel.Id }, readModel);
-            int a = 1, b, c = 0;
-            b = a / c;
-            return Ok();
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            createModel.SysId = Guid.Parse(userId);
+            var readModel = await CreateModel(createModel, cancellationToken);
+            return CreatedAtAction(nameof(Get), new { id = readModel.Id }, readModel);
         }
     }
 }
